613f18f0d62f41cb66b9a2cc3a50a072ba92575b
Ilija Vukotic <ivukotic@cern.ch>
2019-12-23 16:45:16 -0600

    dumper

---
+++ ESdump/dump.js
@@ -0,0 +1,146 @@
+const elasticsearch = require('@elastic/elasticsearch');
+const redis = require('redis');
+
+const testing = true;
+
+console.log('dump starting ... ');
+
+const configPath = './secret/config.json';
+
+const config = require(configPath);
+console.log(config);
+
+const rclient = redis.createClient(config.PORT, config.HOST);
+const es = new elasticsearch.Client({ node: config.ES_HOST, log: 'error' });
+
+async function insert(data) {
+  const result = await es.bulk({ index: 'virtual_placement', body: data });
+  console.log(result.statusCode);
+}
+
+async function storeInES() {
+
+  rclient.keys('*', async function (err, keys) {
+    if (err) return console.log(err);
+
+    console.log('total keys', keys.length)
+
+    const step = 1000;
+    let count = 0;
+    while (count < Math.floor(keys.length / step)) {
+      data = []
+      st = count * step;
+      et = (count + 1) * step;
+      console.log(st, et);
+      for (var i = st; i < et; i++) {
+        const ds = keys[i];
+        // console.log(ds);
+        rclient.lrange(ds, 0, -1, async (err, reply) => {
+          // console.log(ds, reply);
+          data.push({ index: {} }, { ds: ds, placement: reply });
+        });
+      }
+
+      while (data.length < step) {
+        await sleep(50);
+      }
+
+      await insert(data);
+      data = [];
+      count += 1;
+    }
+
+  });
+
+}
+
+
+async function getDisabled() {
+
+  rclient.smembers('disabled_sites', (err, disabled) => {
+    if (err) {
+      console.log('err. sites', err);
+      return;
+    }
+    console.log('disabled sites', disabled);
+    return;
+  });
+
+};
+
+async function getGrid() {
+
+  rclient.smembers('sites', (err, sites) => {
+    if (err) {
+      console.log('err. sites', err);
+      return
+    }
+
+    rclient.mget(sites, (_err, site_cores) => {
+      const cores = {};
+      for (i in sites) {
+        [cloud, site_name] = sites[i].split(':');
+        if (!(cloud in cores)) {
+          cores[cloud] = [];
+        }
+        cores[cloud].push([site_name, Number(site_cores[i])]);
+      }
+      // return cores;
+      console.log(cores);
+    });
+  });
+};
+
+async function getPlacement(dataset) {
+  const ds = dataset;
+  console.log('ds to vp:', ds);
+  rclient.exists(ds, (_err, reply) => {
+    if (reply === 0) {
+      console.log('not found');
+    } else {
+      rclient.lrange(ds, 0, -1, (err, reply) => {
+        console.log("found", reply);
+        return;
+      });
+    }
+  });
+};
+
+function sleep(ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms)
+  })
+}
+
+async function main() {
+  try {
+    await rclient.on('connect', () => {
+      console.log('connected');
+    });
+  } catch (err) {
+    console.error('Error: ', err);
+  }
+  await sleep(300);
+  try {
+    await es.ping(function (err, resp, status) {
+      console.log('ES ping:', resp.statusCode);
+    });
+  } catch (err) {
+    console.error('Error: ', err);
+  }
+
+  await sleep(300);
+
+  const grid = await getGrid()
+
+  await sleep(300);
+  const disabled = await getDisabled();
+
+  await sleep(300);
+
+  console.log('indexing...');
+  storeInES();
+
+}
+
+main();
