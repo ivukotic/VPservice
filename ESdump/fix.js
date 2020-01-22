ab18e25e078f7d7e1e186e3b235795aa85b02ba7
Ilija Vukotic <ivukotic@cern.ch>
2020-01-22 10:34:53 -0600

    fix

--- ESdump/fix.js
+++ ESdump/fix.js
@@ -1,11 +1,11 @@
 const redis = require('redis');
 
 config = {
-  'PORT': 6379,
-  'HOST': 'redis-master.default.svc.cluster.local'
-}
+  PORT: 6379,
+  HOST: 'redis-master.default.svc.cluster.local',
+};
 
-test_keys = [
+const test_keys = [
   'mc16_13TeV:mc16_13TeV.361239.Pythia8EvtGen_A3NNPDF23LO_minbias_inelastic_high.simul.HITS.e4981_s3087_s3111_tid10701335_00',
   'panda:panda.um.user.amoohamm.METPerformance.data.DAOD_JETM3.Nominal.20191210_ZeeEventsLoose.data2018.Zee.v0.41_EMT.log.287688364',
   'mc16_13TeV:mc16_13TeV.312020.Pythia8EvtGen_A14NNPDF23LO_EJ_ModelB_1400_150_4jetFilter.simul.HITS.e7941_e5984_s3126_tid20090961_00',
@@ -16,13 +16,21 @@ test_keys = [
   'mc16_13TeV:mc16_13TeV.410644.PowhegPythia8EvtGen_A14_singletop_schan_lept_top.deriv.DAOD_SUSY7.e6527_e5984_s3126_r10724_r10726_p3703_tid16141402_00',
   'panda:panda.um.user.ychiu.20191215_ce7bd8f4_p94_U.ZdZd13TeV.data2017_NTUP4L.287637795',
   'panda:panda.um.group.phys-higgs.Htt_hh_MMC1.mc16_13TeV.344782.Sh221_PDF30_Ztt_MV140_280_h30h20.D3.e5585_s3126_r9364_p3978.smPre_w_0_HS.286607928',
-  'mc16_13TeV:mc16_13TeV.397570.MGPy8EG_A14N23LO_GG_2500_2490_LLE12k.deriv.DAOD_SUSY2.e7304_e5984_a875_r10201_r10210_p3992_tid19531222_00'
-]
+  'mc16_13TeV:mc16_13TeV.397570.MGPy8EG_A14N23LO_GG_2500_2490_LLE12k.deriv.DAOD_SUSY2.e7304_e5984_a875_r10201_r10210_p3992_tid19531222_00',
+];
 
 const rclient = redis.createClient(config.PORT, config.HOST);
 
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+
 function rename_site(ds, placements, origSite = 'MWT2_DATADISK', newSite = 'MWT2_VP_DISK') {
-  var pos = placements.indexOf(origSite);
+  const pos = placements.indexOf(origSite);
   // console.log(placements, pos);
   if (pos != -1) {
     placements[pos] = newSite;
@@ -45,23 +53,21 @@ function replace_combination(ds, placements, origCombo = ['other', 'other', 'oth
 }
 
 function remove_large(ds, placements) {
-  console.log(placements, len(placements));
-  if (len(placements) > 10) {
+  console.log(placements, placements.length);
+  if (placements.length > 10) {
     rclient.del(ds);
   }
 }
 
 async function test_fix() {
-
-  console.log('total keys', test_keys.length)
+  console.log('total keys', test_keys.length);
 
   let count = 0;
-  while (count < keys.length) {
-
-    const ds = keys[count];
+  while (count < test_keys.length) {
+    const ds = test_keys[count];
     // console.log(ds);
     if (ds.length < 10) {
-      console.log('skipping key', ds)
+      console.log('skipping key', ds);
     }
     rclient.lrange(ds, 0, -1, async (err, reply) => {
       console.log(ds, reply);
@@ -81,19 +87,17 @@ async function test_fix() {
 
 
 async function fix() {
-
   rclient.keys('*', async (err, keys) => {
     if (err) return console.log(err);
 
-    console.log('total keys', keys.length)
+    console.log('total keys', keys.length);
 
     let count = 0;
     while (count < keys.length) {
-
       const ds = keys[count];
       // console.log(ds);
       if (ds.length < 10) {
-        console.log('skipping key', ds)
+        console.log('skipping key', ds);
       }
       rclient.lrange(ds, 0, -1, async (err, reply) => {
         // console.log(ds, reply);
@@ -113,31 +117,25 @@ async function fix() {
         await sleep(60);
       }
     }
-
   });
-
 }
 
 
 async function getDisabled() {
-
   rclient.smembers('disabled_sites', (err, disabled) => {
     if (err) {
       console.log('err. sites', err);
       return;
     }
     console.log('disabled sites', disabled);
-    return;
   });
-
-};
+}
 
 async function getGrid() {
-
   rclient.smembers('sites', (err, sites) => {
     if (err) {
       console.log('err. sites', err);
-      return
+      return;
     }
 
     rclient.mget(sites, (_err, site_cores) => {
@@ -153,7 +151,7 @@ async function getGrid() {
       console.log(cores);
     });
   });
-};
+}
 
 async function getPlacement(dataset) {
   const ds = dataset;
@@ -164,16 +162,9 @@ async function getPlacement(dataset) {
     } else {
       rclient.lrange(ds, 0, -1, (err, reply) => {
         console.log('found', reply);
-        return;
       });
     }
   });
-};
-
-function sleep(ms) {
-  return new Promise(resolve => {
-    setTimeout(resolve, ms)
-  })
 }
 
 async function main() {
@@ -198,7 +189,6 @@ async function main() {
   console.log('fixing...');
   // fix();
   test_fix();
-
 }
 
 main();
