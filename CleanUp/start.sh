1b0c6066556c0fda830e53ce16309e2386c17023
Ilija Vukotic <ivukotic@cern.ch>
2020-04-22 15:13:59 -0500

    fixes

--- CleanUp/start.sh
+++ CleanUp/start.sh
@@ -7,12 +7,6 @@ export CERTPATH=/etc/grid-certs
 
 voms-proxy-init -valid 96:0 -key $CERTPATH/userkey.pem -cert $CERTPATH/usercert.pem --voms=atlas
 
-if [ ! -f /opt/rucio/etc/rucio.cfg ]; then
-    echo "File rucio.cfg not found. It will generate one."
-    mkdir -p /opt/rucio/etc/
-    j2 /rucio.cfg.j2 > /opt/rucio/etc/rucio.cfg
-fi
-
 echo "Enable shell completion on the rucio commands"
 eval "$(register-python-argcomplete rucio)"
 eval "$(register-python-argcomplete rucio-admin)"
