0828576e17a50b831fc2264a2b034bfe74757a50
Ilija Vukotic <ivukotic@cern.ch>
2021-01-14 12:10:06 -0600

    f

--- CleanUp/start.sh
+++ CleanUp/start.sh
@@ -5,6 +5,7 @@ shopt -s checkwinsize
 export CERTPATH=/etc/grid-certs
 
 voms-proxy-init -valid 96:0 -key $CERTPATH/userkey.pem -cert $CERTPATH/usercert.pem --voms=atlas
+chmod 0400 /tmp/x509up_u0
 
 echo "Enable shell completion on the rucio commands"
 eval "$(register-python-argcomplete rucio)"
