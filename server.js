32c1e0792fe0eba20cc568899eb7b9bbd2b53103
Ilija Vukotic <ivukotic@cern.ch>
2022-12-23 19:38:22 -0600

    fixed

--- server.js
+++ server.js
@@ -755,21 +755,21 @@ async function main() {
 
   await subscriber.subscribe('heartbeats', (message) => {
     console.log(`Received heartbeats message: ${message}`);
-    // try {
-    //   const HB = JSON.parse(message);
-    //   // console.log(HB);
-    //   if (!(HB.site in cacheSites)) {
-    //     cacheSites[HB.site] = {};
-    //   }
-    //   if (HB.id in cacheSites[HB.site]) {
-    //     cacheSites[HB.site][HB.id] = HB; // this can't be combined.
-    //   } else {
-    //     cacheSites[HB.site][HB.id] = HB;
-    //     recalculateCluster(HB.site);
-    //   }
-    // } catch (err) {
-    //   console.error('Error in Heartbeats handling: ', err);
-    // }
+    try {
+      const HB = JSON.parse(message);
+      // console.log(HB);
+      if (!(HB.site in cacheSites)) {
+        cacheSites[HB.site] = {};
+      }
+      if (HB.id in cacheSites[HB.site]) {
+        cacheSites[HB.site][HB.id] = HB; // this can't be combined.
+      } else {
+        cacheSites[HB.site][HB.id] = HB;
+        recalculateCluster(HB.site);
+      }
+    } catch (err) {
+      console.error('Error in Heartbeats handling: ', err);
+    }
   });
 
   await subscriber.subscribe('topology', (message) => {
